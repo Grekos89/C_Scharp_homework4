d68d4709edbd83f1d5b3ba1a6f9c4abaf7698d83
Евгений <Grekos_89@mail.ru>
2023-01-26 17:33:24 +0300

    hw2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Принемает на вход число и выдаёт сумму цифр в числе
+int numA = new Random().Next(1, 100000);
+Console.WriteLine(numA);
+Console.WriteLine(GetSum(numA));
+
+static int GetSum(int num)
+{
+    int numSum = 0;
+    while(num > 0)
+    {
+        numSum = numSum + (num % 10);
+        num /= 10;
+    }
+    return numSum;
+}
