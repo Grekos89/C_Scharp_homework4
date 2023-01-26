697e578822070f1fe3aeb2bc943c63043f7c0d72
Евгений <Grekos_89@mail.ru>
2023-01-26 17:32:18 +0300

    hw

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа принемает два числа и возвращает первое в степени второго
+int numA = new Random().Next(1, 10);
+int numB = new Random().Next(1, 10);
+
+Console.WriteLine(GetPow(numA, numB));
+
+static int GetPow(int num1, int num2)
+{
+    int res = 0;
+    res =Convert.ToInt32(Math.Pow(num1, num2));
+    return res;
+}
