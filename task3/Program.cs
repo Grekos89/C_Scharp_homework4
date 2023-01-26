3c9bd742ad6150dcb72a97fdd1ff112f6774bf8f
Евгений <Grekos_89@mail.ru>
2023-01-26 17:33:49 +0300

    hw3

---
+++ task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] newArr = RandomArray(8);
+PrintArray(newArr);
+
+int[] RandomArray(int length)
+{
+     int[]arr = new int[length];
+     for(int i = 0; i < length; i++)
+     {
+         arr[i] = new Random().Next(0,100);
+     }
+     return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    
+    foreach(int item in arr)
+    {
+        System.Console.Write($"{item}, ");
+    } 
+}
